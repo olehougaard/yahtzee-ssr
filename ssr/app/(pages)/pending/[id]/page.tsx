1c9efbc8ceb53ab4bfc13eaecb952090e2e449aa
Ole Ildsgaard Hougaard <oih@via.dk>
2025-11-12 21:22:23 +0100

    Renamed problematic property

--- ssr/app/(pages)/pending/[id]/page.tsx
+++ ssr/app/(pages)/pending/[id]/page.tsx
@@ -3,5 +3,5 @@ import Pending from "@/components/Pending"
 export default async ({params}: {params: Promise<{id: string}>}) => {
   const {id} = await params
 
-  return <Pending id={id}></Pending>
+  return <Pending game_id={id}></Pending>
 }
--- ssr/components/Pending.tsx
+++ ssr/components/Pending.tsx
@@ -6,18 +6,18 @@ import { useGlobalState } from "./StateProvider"
 import { useRouter } from "next/navigation"
 import * as api from '@/src/api'
 
-export default function Pending({id}: {id:string}) {
+export default function Pending({game_id}: {game_id:string}) {
   const router = useRouter()
   
   const { player, ongoing_games, pending_games } = useGlobalState()
-  const game = pending_games.find(g => g.id === id)
+  const game = pending_games.find(g => g.id === game_id)
 
   useEffect(() => {
     if (player === undefined) {
-      router.push(`/login?pending=${id}`)
+      router.push(`/login?pending=${game_id}`)
     } else if (game === undefined) {
-      if (ongoing_games.some(g => g.id === id))
-        router.push(`/game/${id}`)
+      if (ongoing_games.some(g => g.id === game_id))
+        router.push(`/game/${game_id}`)
       else
         router.push('/')
     }
@@ -35,7 +35,7 @@ export default function Pending({id}: {id:string}) {
   }
 
 return <>
-    <h1>Game #{id}</h1>
+    <h1>Game #{game_id}</h1>
     <div>Created by: {game?.creator}</div>
     <div>Players: {game?.players.join(', ')}</div>
     <div>Available Seats: { (game?.number_of_players??2) - (game?.players.length??0)}</div>
