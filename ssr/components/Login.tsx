36865458d0725d3e99e451ccf553f979c4cf9ffd
Ole Ildsgaard Hougaard <oih@via.dk>
2025-11-16 15:30:26 +0100

    useEffect in static page for illustration

--- ssr/components/Login.tsx
+++ ssr/components/Login.tsx
@@ -1,13 +1,16 @@
 'use client'
 
-import { useState, type KeyboardEvent } from "react"
+import { useEffect, useState, type KeyboardEvent } from "react"
 import { useRouter } from 'next/navigation'
 import Cookies from 'js-cookie'
+import { is_finished } from "domain/src/model/yahtzee.game"
+import * as api from '@/src/api'
 
 export default function Login({proceedTo}: {proceedTo: string}) {
   const router = useRouter()
 
   const [player, setPlayer] = useState('')
+  const [gameCount, setGameCount] = useState(0)
 
   const enabled = player !== ''
 
@@ -23,7 +26,18 @@ export default function Login({proceedTo}: {proceedTo: string}) {
     }
   }
 
+  useEffect(() => {
+    async function updateGameCount() {
+      const games = await api.games()
+      console.log(games)
+      const activeGames = games.filter(g => !is_finished(g))
+      setGameCount(activeGames.length)
+    }
+    updateGameCount()
+  })
+
   return <>
+    <p>{gameCount} games playing.</p>
     Username: <input onKeyUp = {loginKeyListener} value={player} onChange={e => setPlayer(e.target.value)}/> <button disabled={!enabled} onClick={login} >Login</button>
     <div></div>
   </>
